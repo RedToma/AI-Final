5ed205e0c401444e6c6be18309947d3fc7b7a3a8
RedToma <jiminkyu0463@gmail.com>
2022-06-14 19:40:18 +0900

    MLP-2

--- MLPfinal.py
+++ MLPfinal.py
@@ -13,6 +13,24 @@ d_target = dd['Outcome'].to_numpy()
 
 x_train, x_test, y_train, y_test = train_test_split(d_input,d_target,train_size=0.6)
 
+param_grid = {'hidden_layer_sizes': [(100),(100,80),(100,80,20)],
+             'learning_rate_init': [0.001, 0.01, 0.1, 1, 10, 100],
+             'batch_size': [30, 60, 100, 200, 300],
+             'max_iter':[100,200,300,400,500]}
+
+mlp = MLPClassifier()
+
+grid_search = GridSearchCV(mlp, param_grid, cv=5, return_train_score=True)
+
+grid_search.fit(x_train, y_train)
+
+print(grid_search.best_params_)
+
+ss = StandardScaler()
+ss.fit(x_train)
+
+x_train = ss.transform(x_train)
+x_test = ss.transform(x_test)
 
 mlp=MLPClassifier(hidden_layer_sizes=(100,80),activation='logistic',
                   learning_rate_init=0.01,batch_size=60,max_iter=500,
